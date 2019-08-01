ef18ac410abb7e37be218d603e326c84b1e210d8
Joshua Hill <josh@jlhworkshop.com>
2019-08-01 01:55:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,10 @@ require "yaml"
 def load_library(file_path)
   emoticons = YAML.load_file(file_path)
   
-  myHash = { 'meaning' => {}, 'emoticon' => {} }
+  myHash = { 
+    'meaning' => {}
+    'emoticon' => {}
+  }
   
   emoticons.each do |key, value|
     myHash['emoticon'][value[0] = emoticons[key][1]
