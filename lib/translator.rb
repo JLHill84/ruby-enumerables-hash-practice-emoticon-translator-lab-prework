549a2eb1e900d7e8c4783105760bb0592ddba184
Joshua Hill <josh@jlhworkshop.com>
2019-08-01 02:01:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,8 @@ def load_library(file_path)
 end
 
 def get_japanese_emoticon(file_path, emoticons)
-  result load_library(file_path)['emoticon']
+  result load_library(file_path)['emoticon'][emoticon]
+  
   
 end
 
