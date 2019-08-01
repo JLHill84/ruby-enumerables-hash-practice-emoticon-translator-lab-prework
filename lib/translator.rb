299e27466b973dec258c18018e32adccb59f003d
Joshua Hill <josh@jlhworkshop.com>
2019-08-01 01:53:54 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(file_path)
   myHash = { 'get_meaning' => {}, 'get_emoticon' => {} }
   
   emoticons.each do |key, value|
-    myHash['get_emoticon'][value[0]] = emoticons
+    myHash['get_emoticon'][value[0]] = emoticons[key]
 end
 
 def get_japanese_emoticon
